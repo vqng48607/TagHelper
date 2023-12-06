8bf16b9fd5bc9987dacb5a33803a0c45ad6bcd88
Vuong <vuonquocnguyen2604@gmail.com>
2023-12-06 12:12:27 +0100

    tagHjelper

---
+++ TagHelper/Controllers/ContactController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using TagHelper.Models;
+
+namespace TagHelper.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly ILogger<ContactController> _logger;
+
+        public ContactController(ILogger<ContactController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string pathData = "Data/collection.json";
+
+            using var jsonFile = System.IO.File.OpenRead(pathData);
+
+            var contacts = JsonSerializer.Deserialize<List<Contact>>(jsonFile);
+
+            if (contacts == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacts);
+
+        }
+
+        public IActionResult Detail(Guid? id)
+        {
+            string pathData = "Data/collection.json";
+
+            using var jsonFile = System.IO.File.OpenRead(pathData);
+
+            var contacts = JsonSerializer.Deserialize<List<Contact>>(jsonFile);
+
+            if (id == null || contacts == null)
+            {
+                return NotFound();
+            }
+
+            var contact = contacts.FirstOrDefault(x => x.Id == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return View(contact);
+        }
+
+    }
+}
